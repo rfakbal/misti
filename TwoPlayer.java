615a0f67383af06b89182432022fd22759ba70f4
RusenAkbal <rusenfirat@gmail.com>
2023-05-14 04:46:55 +0300

    Top scores list changed

--- TwoPlayer.java
+++ TwoPlayer.java
@@ -1,9 +1,11 @@
-import java.io.IOException;
-import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Formatter;
-import java.io.FileWriter;
 import java.util.Scanner;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.io.IOException;
+import java.nio.file.Path;
 
 public class TwoPlayer extends Game{
     boolean ifPrint = false;
@@ -49,100 +51,69 @@ public class TwoPlayer extends Game{
     }
 
     @Override
-    public void scoreBoardAdd(Player p){
-        boolean stop = false;
-        Scanner reader = null;
-        Scanner reader1 = null;
-
+    public void scoreBoardAdd(Player p) {
         String name = p.getPlayerName();
-        String name_updated = name.replace(" ","");
         int score = p.getPlayerScore();
-        String player_expertise = p.getType();
-        int[] scores = new int[10];
-        int[] scores_last  = new int[11];
-        String[] names = new String[10];
-        String[] names_last = new String[11];
-        String[] expertise = new String[10];
-        String[] expertise_last = new String[11];
-        String line = "";
-        String[] line_arr = new String[3];
-        
-        try{
-            reader = new Scanner(Paths.get("Scores.txt"));
-            for(int i = 0;i<10;i++ ){
-                line = reader.nextLine();
-                line_arr = line.split(" ");
-                scores[i] = Integer.valueOf(line_arr[1]);
-                names[i] = line_arr[0];
-                expertise[i] = line_arr[2];
-             }
+        String expertise = p.getType();
+        ArrayList<Integer> scores = new ArrayList<Integer>();
+        ArrayList<String> names = new ArrayList<String>();
+        ArrayList<String> expertises = new ArrayList<String>();
+
+        try {
+            Path file = Paths.get("Scores.txt");
+            if (!Files.exists(file)) {
+                Files.createFile(file);
+            }
+        } catch (IOException e) {
+        System.err.println("");
         }
-        
-        catch(Exception e){
-            System.out.println("");
-        }
-        
-        finally{
-            if(reader != null) reader.close();
-        }
-        
-        int temp = 0;
-        String temp1 = "";
-        String temp2 = "";
-        
-        for(int i = 0;i<10;i++){
-            if(score>scores[i]&&!stop){
-                temp = scores[i];
-                temp1 = names[i];
-                temp2 = expertise[i];
-                scores_last[i+1] = temp;
-                names_last[i+1] = temp1;
-                expertise_last[i+1] = temp2;
-                names_last[i] = name_updated;
-                scores_last[i] = score;
-                expertise_last[i] = player_expertise; 
-                for(int k = 0;k<i;k++){
-                    names_last[k] = names[k];
-                    scores_last[k] = scores[k];
-                    expertise_last[k] = expertise[k];
+
+        try (Scanner scanner = new Scanner(Paths.get("Scores.txt"))) {
+            while (scanner.hasNext()) {
+                String line = scanner.nextLine();
+                String[] lineArr = line.split(" ");
+                if (lineArr.length == 3) {
+                    names.add(lineArr[0]);
+                    scores.add(Integer.parseInt(lineArr[1]));
+                    expertises.add(lineArr[2]);
                 }
-                for(int j = i+2;j<11-i;j++){
-                    names_last[j] = names[j-1];
-                    scores_last[j] = scores[j-1];
-                    expertise_last[j] = expertise[j-1];
-                }
-                stop = true;
             }
-        }
-        if(ifPrint){
-            for(int i = 0;i<10;i++){
-                System.out.println((i+1)+"-) "+names_last[i]+" "+scores_last[i]+" "+ expertise_last[i]);
-            }
-        }
-
-        Formatter f = null;
-        FileWriter fw = null;
-
-        try{
-            fw = new FileWriter("Scores.txt",false);
-            reader1 = new Scanner(Paths.get("Scores.txt"));
-            f = new Formatter(fw);
-            for(int i = 0;i<10;i++){
-                f.format("%s %d %s \n",names_last[i],scores_last[i],expertise_last[i]);
-            }
-        }
-
-        catch(Exception e){
+        } catch (Exception e) {
             System.err.println("");
+            return;
         }
 
-        finally{
-            if(f!=null){
-                f.close();
+        scores.add(score);
+        names.add(name);
+        expertises.add(expertise);
+
+        ArrayList<Integer> topScores = new ArrayList<Integer>(scores);
+        Collections.sort(topScores, Collections.reverseOrder());
+        topScores = new ArrayList<Integer>(topScores.subList(0, Math.min(topScores.size(), 10)));
+
+        ArrayList<Integer> topIndexes = new ArrayList<Integer>();
+        ArrayList<String> topNames = new ArrayList<String>();
+        ArrayList<String> topExpertises = new ArrayList<String>();
+        for (int i = 0; i < topScores.size(); i++) {
+            int index = scores.indexOf(topScores.get(i));
+            topIndexes.add(index);
+            topNames.add(names.get(index));
+            topExpertises.add(expertises.get(index));
+        }
+
+        try (Formatter formatter = new Formatter("Scores.txt")) {
+            for (int i = 0; i < topScores.size(); i++) {
+                formatter.format("%s %d %s %n", topNames.get(i), topScores.get(i),expertises.get(i));
+            }
+        } catch (IOException e) {
+        System.err.println("");
+        }
+        for (int i = 0; i < topScores.size(); i++) {
+            if(ifPrint){
+            System.out.println((i+1)+" -) "+topNames.get(i)+" "+topScores.get(i)+" "+expertises.get(i)); 
             }
         }
-
-    }
+    }   
 
     @Override
     public void drawScoreBoard(){
